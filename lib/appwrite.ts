092b2b34a969b077e0da3dc5738a80f0f4ac8ff5
byambadorj2 <byambadorj993@gmail.com>
2025-07-21 19:37:43 +0800

    setup appwrite

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  platform: "com.jsm.foodordering",
+  project: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+};
